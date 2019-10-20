fea58ddf719319e1e3575168c0aab91d80a814f9
Olga Wojtaszko <olga.jackowska@wp.pl>
2019-10-20 10:37:26 +0200

    class Book - dodane pola

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,13 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+
 public class Book {
+
+    private String title;
+    private String author;
+    private Long idBook;
+
 }
