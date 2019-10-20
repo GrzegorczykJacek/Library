7ace24f68b0214e9d8d8cc08f039665dc05656a1
Pawel Juszczak <paweljuszczak@gmail.com>
2019-10-20 13:20:52 +0200

    Test addRental

--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -1,5 +1,9 @@
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 public class LibraryTest {
 
@@ -13,5 +17,23 @@ public class LibraryTest {
     Book book5 = new Book("Ziarno prawdy", "Zygmunt Miłoszewski", 132L);
     Book book6 = new Book("Uwikłanie", "Zygmunt Miłoszewski", 133L);
 
-    
+    @Test
+    public void testAddRental(){
+        //given
+        Library library = new Library();
+        Adress adress1 = new Adress("Polna","Lublin","20-860","22","33");
+        User user1 = new User(1L,"Jan","Kowalski","janek@wp.pl",adress1);
+        Book book1 = new Book("Tylko martwi nie kłamią", "Katarzyna Bonda", 121L);
+
+
+        //when
+        library.addRental(book1,user1);
+
+        //then
+        assertEquals(1,library.rentalsList.size());
+
+
+
+
+    }
 }
