8b2333488dee9dda38f4d7728cc3511195201da0
Olga Wojtaszko <olga.jackowska@wp.pl>
2019-10-20 11:46:51 +0200

    class Library - dodanie metod na List books

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -5,8 +5,18 @@ public class Library {
 
     List<Rental> rentals = new ArrayList<>(); // Jacek
     List<User> users = new ArrayList<>(); // Paweł
-    List<Book> books = new ArrayList<>(); // Olga
 
 
+    private List<Book> books = new ArrayList<>(); // Olga
+
+    //dodawanie nowych książek do zbioru dostępnych w bibliotece
+    public void addBook(Book book){
+        books.add(book);
+    }
+
+    //usuwanie książek ze zbioru dostępnych w bibliotece
+    public void removeBook(Book book){
+        books.remove(book);
+    }
 
 }
--- src/test/java/Library.java
+++ src/test/java/Library.java
@@ -5,6 +5,16 @@ public class Library {
 
     List<Rental> rentals = new ArrayList<>();
     List<User> users = new ArrayList<>();
+
+
     List<Book> books = new ArrayList<>();
 
+
+    //given
+    Book book1 = new Book("Tylko martwi nie kłamią", "Katarzyna Bonda", 121L);
+    Book book2 = new Book("Pochłaniacz", "Katarzyna Bonda", 122L);
+    Book book3 = new Book("Okularnik", "Katarzyna Bonda", 123L);
+    Book book4 = new Book("Gniew", "Zygmunt Miłoszewski", 131L);
+    Book book5 = new Book("Ziarno prawdy", "Zygmunt Miłoszewski", 132L);
+    Book book6 = new Book("Uwikłanie", "Zygmunt Miłoszewski", 133L);
 }
