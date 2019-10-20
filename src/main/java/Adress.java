a90a29c8902f64e92de5d06f06ef99081e93b234
Pawel Juszczak <paweljuszczak@gmail.com>
2019-10-20 11:12:07 +0200

    dodanie klasy adres i user

---
+++ src/main/java/Adress.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Adress {
+    private String street;
+    private String city;
+    private String postCode;
+    private String houseNumber;
+    private String apartmentNumber;
+
+
+
+}
--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,2 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class User {
+    private Long idUser;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Adress adress;
 }
