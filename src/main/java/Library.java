c1de7c0d091abd81869141101e701042d7a5132d
Jacek Grzegorczyk <jacekgrzegorczyk@op.pl>
2019-10-20 11:51:16 +0200

    Adds methods to Library: addRental()

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Library {
 
@@ -7,6 +8,15 @@ public class Library {
     List<User> users = new ArrayList<>(); // Paweł
     List<Book> books = new ArrayList<>(); // Olga
 
-
+    public void addRental(Book book, User user){
+        Rental rental = new Rental(user.getIdUser(), book.getIdBook());
+        Optional<Rental> foundRental = rentals.stream()
+                .filter(a -> a.getBookID().equals(book.getIdBook()))
+                .findAny();
+                if(!foundRental.isPresent()){
+                    rentals.add(rental);
+                    System.out.println("Nowe wypożyczenie!");
+                }
+    }
 
 }
--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -9,12 +9,19 @@ public class Rental {
     private Long userID;
     private Long bookID;
     private LocalDate rentalStart;
-    private Date rentalEnd = null;
+    private Date rentalDeadline;
 
+    // Adds new book rental
     public Rental(Long userID, Long bookID){
         this.userID = userID;
         this.bookID = bookID;
         this.rentalStart = LocalDate.now();
+        this.rentalDeadline = java.sql.Date.valueOf(rentalStart.plusDays(30));
+    }
+
+    // Returns String with rental info
+    public String toString(){
+        return "Wypożyczono książkę o id: " + bookID + " użytkownikowi o id: " + userID;
     }
 
 }
