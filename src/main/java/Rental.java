e2b4b64bedbd4f0cd3c82a49326b8e06bbb15fd7
Jacek Grzegorczyk <jacekgrzegorczyk@op.pl>
2019-10-20 11:07:57 +0200

    Adds Library class

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,2 +1,20 @@
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+@Getter
 public class Rental {
+
+    private Long userID;
+    private Long bookID;
+    private LocalDate rentalStart;
+    private Date rentalEnd = null;
+
+    public Rental(Long userID, Long bookID){
+        this.userID = userID;
+        this.bookID = bookID;
+        this.rentalStart = LocalDate.now();
+    }
+
 }
---
+++ src/test/java/Library.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+
+    List<Rental> rentals = new ArrayList<>();
+    List<User> users = new ArrayList<>();
+    List<Book> books = new ArrayList<>();
+
+}
