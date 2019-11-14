65cf5249ef53e3a6ac157ff64814da43d5e85d6a
Jacek Grzegorczyk <jacekgrzegorczyk@op.pl>
2019-11-14 23:02:55 +0100

    Adds: introduction window on startup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
 import java.io.FileNotFoundException;
 import java.text.ParseException;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
+
     public static void main(String[] args) throws FileNotFoundException, ParseException {
+
+        JFrame introFrame = new JFrame("Library 1.0");
+        int screenWidth = Toolkit.getDefaultToolkit().getScreenSize().width;
+        int screenHeight = Toolkit.getDefaultToolkit().getScreenSize().height;
+        introFrame.setBounds(((screenWidth / 2) - 150), ((screenHeight / 2) - 75), 300, 150);
+        introFrame.setResizable(false);
+        introFrame.setIconImage(Toolkit.getDefaultToolkit().getImage("book.jpg"));
+        introFrame.setDefaultCloseOperation(3);
+        introFrame.setVisible(true);
+
+
 
         System.out.println("Library 1.0\n" +
                 "Program zarządza biblioteką książek za pomocą prostego MENU\n" +
@@ -10,6 +24,8 @@ public class Main {
 
         UserMenu userMenu = new UserMenu();
         userMenu.printMenu();
+
+
 
     }
 }
